3dac4c25a538bded518d835f6e70477fe817c885
Rune Pollet <runepollet@icloud.com>
2022-02-08 15:12:03 +0100

    Initial Commit

---
+++ Library app/Library_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Library_appApp.swift
+//  Library app
+//
+//  Created by Rune Pollet on 08/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct Library_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
